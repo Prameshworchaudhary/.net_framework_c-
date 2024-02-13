a104523bbc40a22dedcf5a364fa96e6697f320cb
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-13 12:11:00 +0545

    constructor_default

---
+++ demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ncc_lab_prameshwor
+{
+    class animal
+    {
+        public animal()
+        {
+            Console.WriteLine("iam animal");
+        }
+    }
+    internal class demo
+    {
+        static void Main(string[] args)
+        {
+            animal cow = new animal();
+        }
+    }
+}
