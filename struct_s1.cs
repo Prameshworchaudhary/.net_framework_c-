935eaa4754c20f7bdc608ed1238402a0bf4e6b5e
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-13 12:29:24 +0545

    struct

---
+++ struct_s1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ncc_lab_prameshwor
+{
+
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+
+    
+    internal class struct_s1
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3,4);
+            Console.WriteLine($"Point p1: {p1}");
+        }
+
+    }
+}
